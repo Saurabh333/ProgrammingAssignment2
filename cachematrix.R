94d8be4dd8bd89deef03ad6334e60c10ba842243
Saurabh333 <event.horizon786@gmail.com>
2015-01-26 03:25:59 +0530

    This prog computes the inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,38 +1,44 @@
 ## This function calculates the mean of the matrix and if the inverse has already been calculated then it gets
 ## inverse from cache an dskips computation.
 
-## Write a short comment describing this function
+## This function is used to 
+## 1 Set the value of matrix
+## 2 Get the value of matrix
+## 3 Set the value of inverse
+## 4 Get the value of inverse
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
-  print(environment())
-  evn <- environment()
-  print(parent.env(evn))
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
+print(parent.env(evn))
+set <- function(y) {
+x <<- y
+m <<- NULL
 
 }
-setsolve <- function(solve) m <<- solve
+setsolve <- function(solve) m <<- solve   ## This function sets the inverted matrix
 getsolve <- function() m
-getevn<- function() environment()
+##getevn<- function() environment()
 list(set = set, get = get,
      setsolve = setsolve,
-     getsolve = getsolve,
-     getevn = getevn)
+     getsolve = getsolve)## This function fetches the inverted matrix
+    
 }
 
 
-## Write a short comment describing this function
+## # used to get the cache of the matrix
 
-cacheSolve <- function(x) {
+cacheSolve <- function(x=matrix(),..) {
+  x<-matrix()
   m<-x$getsolve()
-  if(!is.null(m)){
-    message("Getting cached data")
+  if(!is.null(m)){   ## If there is data (matrix)
+    if(x$setmatrix()==x$getmatrix()){
+    message("Getting cached data")   ## If condition is true
     return(m)
   }
-  data<-x$get()
-  m<-solve(data)
-  x$setsolve(m)
+  }
+  #if the inverse if not there, first it is calculated and then retrieved.
+  y<-x$get()  ## If the condition is False 
+  m<-solve(x,..)  ## This calculates the inverse of matrix
+  x$setsolve(m)  
   m
-  }   
+  }
